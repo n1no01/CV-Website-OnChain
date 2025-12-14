0b58eb42a8cb735bbdc995c2e960f6cc0da74393
Nino Herenda <ninogor@gmail.com>
2025-12-14 12:01:19 +0100

    Modify description

---
+++ juno.config.js
@@ -0,0 +1,9 @@
+import {defineConfig} from '@junobuild/config';
+
+/** @type {import('@junobuild/config').JunoConfig} */
+export default defineConfig({
+  satellite: {
+    id: '332zf-5qaaa-aaaal-ajb7q-cai',
+    source: 'dist'
+  }
+});
