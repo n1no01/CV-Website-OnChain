e7422403c60ca1cf879b2677b63d280e8584d19b
Nino Herenda <ninogor@gmail.com>
2026-05-12 22:21:42 +0200

    Website Redesign

--- dist/script.js
+++ dist/script.js
@@ -1,12 +1,62 @@
-window.addEventListener("DOMContentLoaded", function () {
-    VANTA.NET({
-        el: "body",
-        mouseControls: true,
-        touchControls: true,
-        gyroControls: false,
-        color: 0x1f1f1f,
-        backgroundColor: 0x050505,
-        minHeight: window.innerHeight,
-        minWidth: window.innerWidth
+const track = document.querySelector(".carousel-track");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+
+function getSlideWidth() {
+
+    const slide = document.querySelector(".project-slide");
+
+    const slideStyle = window.getComputedStyle(slide);
+
+    const gap =
+        parseInt(slideStyle.marginRight) || 25;
+
+    return slide.offsetWidth + gap;
+}
+
+/* NEXT */
+
+nextBtn.addEventListener("click", () => {
+
+    track.scrollBy({
+        left: getSlideWidth(),
+        behavior: "smooth"
     });
+
 });
+
+/* PREVIOUS */
+
+prevBtn.addEventListener("click", () => {
+
+    track.scrollBy({
+        left: -getSlideWidth(),
+        behavior: "smooth"
+    });
+
+});
+
+/* AUTOPLAY */
+
+setInterval(() => {
+
+    const maxScrollLeft =
+        track.scrollWidth - track.clientWidth;
+
+    if (track.scrollLeft >= maxScrollLeft - 10) {
+
+        track.scrollTo({
+            left: 0,
+            behavior: "smooth"
+        });
+
+    } else {
+
+        track.scrollBy({
+            left: getSlideWidth(),
+            behavior: "smooth"
+        });
+
+    }
+
+}, 4000);
--- script.js
+++ script.js
@@ -1,4 +1,62 @@
-function ScrollToSkills(){
-    const profilePhoto = document.querySelector(".mySkills");
-    profilePhoto.scrollIntoView(true);
+const track = document.querySelector(".carousel-track");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+
+function getSlideWidth() {
+
+    const slide = document.querySelector(".project-slide");
+
+    const slideStyle = window.getComputedStyle(slide);
+
+    const gap =
+        parseInt(slideStyle.marginRight) || 25;
+
+    return slide.offsetWidth + gap;
 }
+
+/* NEXT */
+
+nextBtn.addEventListener("click", () => {
+
+    track.scrollBy({
+        left: getSlideWidth(),
+        behavior: "smooth"
+    });
+
+});
+
+/* PREVIOUS */
+
+prevBtn.addEventListener("click", () => {
+
+    track.scrollBy({
+        left: -getSlideWidth(),
+        behavior: "smooth"
+    });
+
+});
+
+/* AUTOPLAY */
+
+setInterval(() => {
+
+    const maxScrollLeft =
+        track.scrollWidth - track.clientWidth;
+
+    if (track.scrollLeft >= maxScrollLeft - 10) {
+
+        track.scrollTo({
+            left: 0,
+            behavior: "smooth"
+        });
+
+    } else {
+
+        track.scrollBy({
+            left: getSlideWidth(),
+            behavior: "smooth"
+        });
+
+    }
+
+}, 4000);
